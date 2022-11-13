54cc9b9b8c010876b40e383b58f6c40942c3de5a
prnvyellayi <prnvshanmukh00@gmail.com>
2022-11-14 00:49:40 +0530

    first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { saveAs } from 'file-saver';
+import {useState} from 'react'
 
 function App() {
+
+  const [fileEvent, setFileEvent] = useState("")
+  const [isFileSelected, setSelected] = useState(false)
+
+  const onChangeFile = () => {
+    const zip = require('jszip')();
+    let files = fileEvent.target.files;
+    for (let file = 0; file < fileEvent.target.files.length; file++) {
+      // Zip file with the file name.
+      zip.file(files[file].name, files[file]);
+    }
+    zip.generateAsync({ type: "blob" }).then(content => {
+      saveAs(content, "download.zip");
+    });
+  }
+
+  const onUpload = (event) => {
+    setFileEvent(event)
+    setSelected(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='div2'>
+        <h1 style={{ fontSize: "40px", marginBottom: "40px" }}>Upload file(s)</h1>
+        <div>
+          <input multiple type="file" name="file" className='inputStyle' onChange={onUpload}/>
+        </div>
+        <button type="button" className='buttonStyle' disabled={!isFileSelected} onClick={onChangeFile}>
+          Download
+        </button>
+      </div>
     </div>
   );
 }
